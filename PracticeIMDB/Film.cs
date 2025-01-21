6bcf1753e906ed13fcb5225fd590e7997221c00a
Senanur Yavuzarslan <yavuzarslansenanur@gmail.com>
2025-01-22 01:30:29 +0300

    Add project files.

---
+++ PracticeIMDB/Film.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeIMDB
+{
+    public class Film
+    {
+        public string FilmAdi { get; set; }
+        public double ImdbPuani { get; set; }
+        
+        public Film(string filmAdi, double imdbPuani)
+        {
+            FilmAdi = filmAdi;
+            ImdbPuani = imdbPuani;
+        }
+    }
+}
---
+++ PracticeIMDB/Program.cs
@@ -0,0 +1,54 @@
+﻿using PracticeIMDB;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Film> filmler = new List<Film>();
+
+        string filmAdi;
+        double imdbPuani;
+        string filmEkle;
+        do
+        {
+            Console.Write("\nFilmin adını giriniz: ");
+            filmAdi = Console.ReadLine();
+
+            Console.Write("Filmin IMDb puanını giriniz: ");
+            imdbPuani = Convert.ToDouble(Console.ReadLine());
+
+            filmler.Add(new Film(filmAdi,imdbPuani));
+
+            Console.Write("\nBaşka bir film eklemek istiyor musunuz? (Evet/Hayır): ");
+            filmEkle = Console.ReadLine().ToLower();
+
+        } while (filmEkle == "evet");
+
+
+        Console.WriteLine("\n-----------------Bütün Filmler----------------");
+        foreach (Film film in filmler)
+        {
+            Console.WriteLine("Film Adı: " + film.FilmAdi + " IMDb: " + film.ImdbPuani);
+        }
+
+        Console.WriteLine("\n--------IMDb 4-9 Arasında Olan Filmler--------");
+        foreach (Film film in filmler)
+        {
+            if (film.ImdbPuani >= 4 && film.ImdbPuani <= 9)
+            {
+                Console.WriteLine("Film Adı: " + film.FilmAdi + " IMDb: " + film.ImdbPuani);
+            }            
+        }
+
+        Console.WriteLine("\n--------İsmi 'A' ile Başlayan Filmler---------");
+        foreach (Film film in filmler)
+        {
+            if (film.FilmAdi.StartsWith("A") || film.FilmAdi.StartsWith("a"))
+            {
+                Console.WriteLine("Film Adı: " + film.FilmAdi + " IMDb: " + film.ImdbPuani);
+            }
+        }
+
+    }
+}
+
